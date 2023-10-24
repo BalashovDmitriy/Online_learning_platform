5280571f2093ac0b22946e94b4450f7aabda0e87
dmitriy.balashov <reaver_std@mail.ru>
2023-10-24 12:07:04 +0500

    24.1 step-1

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=25, **NULLABLE)
+    city = models.CharField(max_length=100, **NULLABLE)
+    avatar = models.ImageField(upload_to="avatars", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
