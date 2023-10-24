1e61d6eb754af247fdc78917d09b9c1ea0570329
dmitriy.balashov <reaver_std@mail.ru>
2023-10-24 12:19:42 +0500

    24.1 step-2

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('education.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
