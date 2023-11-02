4d5ac4175a400ba037248a934742f9f9a468926c
dmitriy.balashov <reaver_std@mail.ru>
2023-11-02 13:02:25 +0500

    homework 25.2 step 1

---
+++ education/validators.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class LinkValidator:
+    def __init__(self, link):
+        self.link = link
+
+    def __call__(self, value):
+        """ Проверка вхождения youtube.com в ссылке"""
+        if dict(value).get(self.link) and 'youtube.com' not in dict(value).get(self.link).split('/'):
+            raise serializers.ValidationError('Ссылка на видео должна быть на youtube.com')
