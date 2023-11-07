193080bc29de3d379159ba608bd7f2c79e23c8e3
dmitriy.balashov <reaver_std@mail.ru>
2023-11-07 15:55:32 +0500

    homework 26.1 add sending payment link to user email

--- education/services.py
+++ education/services.py
@@ -57,10 +57,19 @@ def get_session(serializer: Payment):
         mode='payment',
         customer_email=serializer.user.email
     )
-    print(session.__dict__)
+    send_payment_link_to_mail(session.url, serializer.user.email)
     return session
 
 
 def retrieve_session(session):
     """ Получаем детали сессии"""
     return stripe.checkout.Session.retrieve(session)
+
+
+def send_payment_link_to_mail(url, email):
+    send_mail(
+        subject='Оплата курса',
+        message=f'Ссылка для оплаты курса: {url}',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+    )
